0e24ea691fb692f11daaf3256e6d8303559848ef
Sergey Kolesnikov <scitator@gmail.com>
2021-07-15 08:49:13 +0300

    Initial commit

---
+++ hydra_slayer/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from hydra_slayer.__version__ import __version__
---
+++ setup.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# flake8: noqa
+# -*- coding: utf-8 -*-
+
+# Note: To use the "upload" functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+from shutil import rmtree
+import sys
+
+from setuptools import Command, find_packages, setup
+
+# Package meta-data.
+NAME = "hydra-slayer"
+DESCRIPTION = "Hydra Slayer."
+URL = ""
+EMAIL = ""
+AUTHOR = ""
+REQUIRES_PYTHON = ">=3.6.0"
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_requirements(filename):    
+    with open(os.path.join(PROJECT_ROOT, filename), "r") as f:
+        return f.read().splitlines()
+
+
+def load_readme():
+    readme_path = os.path.join(PROJECT_ROOT, "README.md")
+    with io.open(readme_path, encoding="utf-8") as f:
+        return f"\n{f.read()}"
+
+
+def load_version():
+    context = {}
+    with open(os.path.join(PROJECT_ROOT, "hydra_slayer", "__version__.py")) as f:
+        exec(f.read(), context)
+    return context["__version__"]
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(PROJECT_ROOT, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(load_version()))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Specific dependencies.
+extras = {}
+
+setup(
+    name=NAME,
+    version=load_version(),
+    description=DESCRIPTION,
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    keywords=["Distributed Computing"],
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    download_url=URL,
+    project_urls={},
+    packages=find_packages(exclude=("tests",)),
+    entry_points={},
+    scripts=[],
+    install_requires=load_requirements("requirements/requirements.txt"),
+    extras_require=extras,
+    include_package_data=True,
+    license="Apache License 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "Natural Language :: English",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+        # Audience
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        # Topics
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        # Programming
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
+)
